c997e6ceb395d8f5bd69621ece45bb7825d0c4e3
FelipePereiraBL <felipereirabl@gmail.com>
2021-08-17 23:16:50 -0300

    Esboço do controller da tela principal

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,27 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController 
+{
+	@FXML
+	private MenuItem menuIteminventory;
+	@FXML
+	private MenuItem menuItemCategoryProducts;
+	
+	//Abrir tela de estoque
+	@FXML
+	public void onMenuItemInventory()
+	{
+		
+	}
+	
+	//Abrir tela de categoria de produtos
+	@FXML
+	public void onMenuItemCategoryProducts()
+	{
+		
+	}
+
+}
