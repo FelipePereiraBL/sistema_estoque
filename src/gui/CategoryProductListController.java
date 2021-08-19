4ebacf0794c43a645560ebef7afb1b2ad094efc4
FelipePereiraBL <felipereirabl@gmail.com>
2021-08-19 16:35:00 -0300

    createDialogForm(cria uma janela de dialogo para o formulario)

--- src/gui/CategoryProductListController.java
+++ src/gui/CategoryProductListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.CategoryProduct;
 import model.services.CategoryProductService;
@@ -40,8 +49,10 @@ public class CategoryProductListController implements Initializable
 		this.services = services;
 	}
 	
-	public void onBtNewCategoryProduct()
+	public void onBtNewCategoryProduct(ActionEvent event)
 	{
+		Stage parentStage=Utils.currentStage(event);
+		createDialogForm("/gui/CategoryProductForm.fxml",parentStage);
 
 	}
 
@@ -75,5 +86,29 @@ public class CategoryProductListController implements Initializable
 		 obsList=FXCollections.observableArrayList(list);
 		 tableViewCategoriesProducts.setItems(obsList);
 	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage)
+	{
+		try 
+		{
+			FXMLLoader loader=new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane=loader.load();
+			
+			Stage dialogStage=new Stage();
+			dialogStage.setTitle("Enter department data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		} 
+		catch (IOException e) 
+		{
+		     e.getStackTrace();
+			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+		
+	}
+
 
 }
